6e130afe3fb6e7e8fb9251e397f77b182e4826dc
AshashaEmover <ashashaorl08@gmail.com>
2024-12-04 07:57:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import kivy
-from kivy.app import App #создание приложения
-from kivy.uix.boxlayout import BoxLayout #автоматическое распределение пространства между виджетами
-from kivy.uix.label import Label #текст 
-from kivy.uix.textinput import TextInput #поле ввода
-from kivy.uix.button import Button #кнопки
-from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition #слои приложения, экраны, переход между экранами(замена)
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 
-from kivy.core.clipboard import Clipboard #копирование текста
-from kivy.properties import ObjectProperty, StringProperty #связывание свойств объекта
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.core.text import LabelBase
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.core.clipboard import Clipboard
+from kivy.properties import ObjectProperty, StringProperty
 
-from kivy.config import Config #дизайн
-Config.set('kivy', 'keyboard_mode', 'systemanddock')
+### Настройки ###
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'systemanddock')  # Активация клавы на Андройде
 
-# Определение русского алфавита
-RUSSIAN_ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-# Определение английского алфавита
-ENGLISH_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LabelBase.register(name='NadejdaBold', fn_regular='NadejdaBold.ttf')  # Добавление шрифтов
+LabelBase.register(name='TimesNewRoman', fn_regular='TimesNewRoman.ttf')
+
+# Window.size = (1080 / 2.8, 2048 / 2.8) # Стартовое размер окна на компе (закомментить для Андройда)
+
+RUSSIAN_ALPHABET = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'  # Определение русского алфавита
+ENGLISH_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # Определение английского алфавита
 
 
+### Движок ###
 def shifrovanie(text, key, alphabet):
     mytext = ''
     key_length = len(key)
@@ -30,12 +40,12 @@ def shifrovanie(text, key, alphabet):
     for symb in text:
         if symb in alphabet:
             text_index = alphabet.index(symb)
-            key_index = alphabet.index(key[j % key_length]) #цикличное использование ключа
+            key_index = alphabet.index(key[j % key_length])
             encrypted_index = (text_index + key_index) % len(alphabet)
             mytext += alphabet[encrypted_index]
+            j += 1
         else:
             mytext += symb  # Если символ не в алфавите, добавляем его как есть
-        j += 1
     return mytext.lower()  # Возвращаем зашифрованный текст в нижнем регистре
 
 
@@ -54,21 +64,169 @@ def deshifrovanie(text, key, alphabet):
             key_index = alphabet.index(key[j % key_length])
             encrypted_index = (text_index - key_index) % len(alphabet)
             mytext += alphabet[encrypted_index]
+            j += 1
         else:
             mytext += symb
-        j += 1
     return mytext.lower()
 
 
-class Container(BoxLayout):
+class SimpleTextInput(TextInput):
+    def on_double_tap(self):
+        pass
+
+    def on_triple_tap(self):
+        pass
+
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+### Конструктор ###
+Builder.load_string('''
+#:import Clipboard kivy.core.clipboard.Clipboard
+
+# Экран выбора языка
+<LanguageSelectionScreen>: 
+
+    BoxLayout:  # Основная рабочая область
+        orientation: 'vertical'
+        padding: 50
+        spacing: 50
+
+        Label:
+            text: 'Шифрование на каком языке?'
+            font_size: '30sp'
+            font_name: 'NadejdaBold'
+
+        ImageButton:  # Картинка с поведением кнопки
+            size_hint: 1, 1
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            on_press: root.manager.current = 'english'  # На вызове: меняет в Менеджере Экранов текущий экран 
+            text: 'English'
+            # Код с картинкой 
+            canvas:            
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+                    source: 'en_flag.png'
+            # Конец кода с картинкой 
+
+        ImageButton:  # Картинка с поведением кнопки
+            size_hint: 1, 1
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            on_press: root.manager.current = 'russian'  # Нажатие: меняет в Менеджере Экранов текущий экран 
+            text: ''
+            # Код с картинкой 
+            canvas:     
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+                    source: 'ru_flag.png'
+            # Конец кода с картинкой 
+
+# Экран шифра
+<CipherScreen>:
+
+    BoxLayout:  # Основная рабочая область
+        orientation: 'vertical'
+        padding: 20
+        spacing: 20
+        
+        SimpleTextInput:  # Вывод текста с множеством строк с отключенным выделением
+            text: root.result_txt
+            size_hint: 1, 1
+            font_size: '32sp'
+            font_name: 'TimesNewRoman'
+            multiline: True  # много строк
+            background_color: [0,0,0,0]
+            cursor_color: [1,1,1,1]
+            readonly: True
+            allow_copy: False
+            on_focus: cancel_selection: True
+            scroll_from_swipe: True
+            foreground_color: [1,1,1,1]         
+        
+        BoxLayout:  # Доп горизонтальная рабочая область
+            orientation: 'horizontal'
+            padding: 0
+            spacing: 10
+            size_hint: 1, 0.8
+
+            TextInput:  # Ввод текста
+                id: txt_inpt
+                multiline: True
+                hint_text: 'Введите текст'
+                size_hint: 1, 1
+                font_name: 'TimesNewRoman'
+                font_size: '24sp'
+                
+            Button:  # Кнопка удалить
+                size: 40, 40
+                size_hint: None, None
+                pos_hint: {'center_y': 0.5}
+                on_press: root.del_text_inpt('txt_inpt')  # Нажатие: удалить текст у виджета с id txt_inpt
+                text: 'Del'
+                font_size: '14sp'
+                font_name: 'TimesNewRoman'             
+        
+        BoxLayout:  # Доп горизонтальная рабочая область
+            orientation: 'horizontal'
+            padding: 0
+            spacing: 10
+            size_hint: 1, 0.2
+        
+            TextInput:  # Ввод текста
+                id: key_inpt
+                multiline: False
+                hint_text: 'Введите ключ'
+                size_hint: 1, 1
+                font_name: 'TimesNewRoman'
+                font_size: '24sp'
+                
+            Button:  # Кнопка удалить
+                size: 40, 40
+                size_hint: None, None
+                pos_hint: {'center_y': 0.5}
+                on_press: root.del_text_inpt('key_inpt')  # Нажатие: удалить текст у виджета с id txt_inpt
+                text: 'Del'
+                font_size: '14sp'
+                font_name: 'TimesNewRoman' 
+            
+        BoxLayout:  # Доп горизонтальная рабочая область
+            orientation: 'horizontal'
+            padding: 0
+            spacing: 20
+            size_hint: 1, 0.25
+            
+            Button:  # Кнопка Шифровать
+                size_hint: 1, 1
+                on_press: root.encrypt()  # Нажатие: запуск функции движка через функцию родителя
+                text: 'Шифровать'
+                font_size: '30sp'
+                font_name: 'NadejdaBold'
+    
+            Button:  # Кнопка Дешифровать
+                size_hint: 1, 1
+                on_press: root.decrypt()  # Нажатие: запуск функции движка через функцию родителя
+                text: 'Дешифровать'
+                font_size: '30sp'
+                font_name: 'NadejdaBold'
+
+        Button:  # Кнопка Копировать
+            size_hint: 1, 0.25
+            on_release: Clipboard.copy(root.txt_to_copy)  # Нажатие: копировать результат в переменной родителя
+            text: 'Копировать результат'
+            font_size: '30sp'
+            font_name: 'NadejdaBold'
+''')
+
+
+### Kivy ###
+class LanguageSelectionScreen(Screen):  # Экран выбора языка
     pass
 
 
-class LanguageSelectionScreen(Screen):
-    pass
-
-
-class CipherScreen(Screen):
+class CipherScreen(Screen):  # Экран шифра
     result_txt = StringProperty('Результат будет здесь')
     txt_to_copy = StringProperty('')
 
@@ -76,34 +234,47 @@ class CipherScreen(Screen):
         super().__init__(**kwargs)
         self.alphabet = alphabet
 
-    def encrypt(self):
+    def encrypt(self):  # Вызов функции движка
         text = self.ids.txt_inpt.text
         key = self.ids.key_inpt.text
-        if text and key:
-            self.txt_to_copy = shifrovanie(text, key, self.alphabet)
-            self.result_txt = f'Зашифрованный текст: {self.txt_to_copy}'
-        else:
-            self.result_txt = 'Пожалуйста, введите текст и ключ.'
+        try:  # Проверка на посторонние символы в ключе
+            if text and key:
+                self.txt_to_copy = shifrovanie(text, key, self.alphabet)  # Вызов функции движка
+                self.result_txt = f'{self.txt_to_copy}'  # Сохранение текста для копирования
+            else:
+                self.result_txt = 'Пожалуйста, введите текст и ключ.'
+        except ValueError:  # Посторонние символы в ключе
+            self.result_txt = 'Пожалуйста, введите ключ без символов.'
 
-    def decrypt(self):
+    def decrypt(self):  # Вызов функции движка
         text = self.ids.txt_inpt.text
         key = self.ids.key_inpt.text
-        if text and key:
-            self.txt_to_copy = deshifrovanie(text, key, self.alphabet)
-            self.result_txt = f'Расшифрованный текст: {self.txt_to_copy}'
+        try:  # Проверка на посторонние символы в ключе
+            if text and key:
+                self.txt_to_copy = deshifrovanie(text, key, self.alphabet)  # Вызов функции движка
+                self.result_txt = f'{self.txt_to_copy}'  # Сохранение текста для копирования
+            else:
+                self.result_txt = 'Пожалуйста, введите текст и ключ.'
+        except ValueError:  # Посторонние символы в ключе
+            self.result_txt = 'Пожалуйста, введите ключ без символов.'
+
+    def del_text_inpt(self, id):  # Функциия удаления текста
+        if id == 'txt_inpt':
+            self.ids.txt_inpt.text = ''
         else:
-            self.result_txt = 'Пожалуйста, введите текст и ключ.'
+            self.ids.key_inpt.text = ''
 
 
-class CipherApp(App):
+class CipherApp(App):  # Основной класс приложения
     def build(self):
-        sm = ScreenManager(transition=SwapTransition())
+        sm = ScreenManager(transition=SwapTransition())  # Создание Менеджера Экрана
         sm.add_widget(LanguageSelectionScreen(name='language'))
         sm.add_widget(CipherScreen(RUSSIAN_ALPHABET, name='russian'))
         sm.add_widget(CipherScreen(ENGLISH_ALPHABET, name='english'))
 
+        # Код для шага назад при нажатии кнопки назад Андроида или esc Windows
         def post_build_init(ev):
-            from kivy.base import EventLoop #непрерывное исполнение приложения
+            from kivy.base import EventLoop
             EventLoop.window.bind(on_keyboard=hook_keyboard)
 
         def hook_keyboard(window, key, *largs):
@@ -114,9 +285,10 @@ class CipherApp(App):
                 return True
 
         self.bind(on_start=post_build_init)
+        # Конец кода для шага назад при нажатии кнопки назад Андроида или esc Windows
 
         return sm
 
 
 if __name__ == '__main__':
-    CipherApp().run()
+    CipherApp().run()  # Запуск приложения
